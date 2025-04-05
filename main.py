5325aba27c564e0f9795e193ae622323f2b2261d
olssanna123 <57717845+olssanna123@users.noreply.github.com>
2025-04-05 15:32:02 +0200

    Plots randomly generates point in region approximation graphically

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from calculate_travel import distance_matrix
 from generate_data import generate_data
 from region import random_pos_within_region
+from region import region_approximation_graphic
 
 # coordinates for approximation region as tuples
 # North east south west coordinates (latitude, longitude)
-Ale = [(58.116472, 12.397922), (57.990763, 12.410281), (57.805393, 12.016147), (58.002408, 12.130130)]
+Ale_tuples = [(58.116472, 12.397922), (57.990763, 12.410281), (57.805393, 12.016147), (58.002408, 12.130130)]
+
+# North east south west coordinates [latitude, longitude]
+Ale_lists = [[58.116472, 12.397922], [57.990763, 12.410281], [57.805393, 12.016147], [58.002408, 12.130130]]
+
 
 # generate array to sample from
 #sampling_array = generate_data.create_sampling_array()
@@ -14,5 +19,7 @@ Ale = [(58.116472, 12.397922), (57.990763, 12.410281), (57.805393, 12.016147), (
 #distances = distance_matrix.calculate_distances(samples)
 #print(distances)
 
-p = random_pos_within_region.random_point_within_region(Ale)
+p = random_pos_within_region.random_point_within_region(Ale_tuples)
 print(p)
+
+region_approximation_graphic.plot_approximation(Ale_lists, p)
--- region/region_approximation_graphic.py
+++ region/region_approximation_graphic.py
@@ -1,10 +1,7 @@
 import matplotlib.pyplot as plt
 
-# North east south west coordinates [latitude, longitude]
-Ale = [[58.116472, 12.397922], [57.990763, 12.410281], [57.805393, 12.016147], [58.002408, 12.130130]]
-
 # plot form of approximation of region
-def plot_approximation(region):
+def plot_approximation(region, point):
     # create closed loop by repeating the first coordinate
     region.append(region[0])
     # create lists of x and y values
@@ -12,6 +9,5 @@ def plot_approximation(region):
     # plot figure
     plt.figure()
     plt.plot(xs,ys)
+    plt.plot(point[0], point[1], 'r+')
     plt.show() # if you need...
-
-plot_approximation(Ale)
